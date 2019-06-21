d03c37a9a2f5d57cc0b16736a6fb4391624fe35e
rrami <rrami@DESKTOP-09BRSIP>
2019-06-21 14:07:19 -0600

    Modificaciones al 23052019

--- src/main/java/com/antsolution/antcore/controller/seguridad/UsuarioSeguridadController.java
+++ src/main/java/com/antsolution/antcore/controller/seguridad/UsuarioSeguridadController.java
@@ -7,8 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.servlet.http.HttpSession;
 import org.primefaces.event.SelectEvent;
 
 @Named
@@ -35,6 +38,16 @@ public class UsuarioSeguridadController implements Serializable {
         selected = new UsuarioSeguridad();
         selected.setNombreUsuario("");
 
+    }
+    
+    public String logout() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getSessionMap().clear();
+        ExternalContext externalContext = context.getExternalContext();
+        Object session = externalContext.getSession(true);
+        HttpSession httpSession = (HttpSession) session;
+        httpSession.invalidate();
+        return "/login.xhtml?faces-redirect=true";
     }
 
     public void guardar() {
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%-- 
+    Document   : index
+    Created on : 06-21-2019, 01:49:31 PM
+    Author     : RAREcheverria
+--%>
+
+<% response.sendRedirect("inicio.xhtml");%>
